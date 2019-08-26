178ab3809eb3fa92d41560e44a675948f7bd65cb
rafaeljack3 <rafaeljack3@gmail.com>
2019-08-26 17:18:32 -0300

    Create Home.jsx

---
+++ app/static/components/Home.jsx
@@ -0,0 +1,12 @@
+'use strict';
+var React = require('react');
+
+class Home extends React.Component{
+	render(){
+		return (
+        	<h1>Hello FUCK React!</h1>
+       )
+	}
+}
+
+module.exports = Home; 
